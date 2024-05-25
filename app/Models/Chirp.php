c5d90670017781cf04141768b427d31ffa98d651
Fadjrir Herlambang <fadjrir.co.id@gmail.com>
2024-05-25 10:24:32 +0700

    first commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    /**
+     * Get the user that owns the Chirp
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
